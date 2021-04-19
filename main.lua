729266346dfe3138afcb6b6366620af51a5ba2e4
Paige Seivertson <pzeig66@yahoo.com>
2021-04-18 23:11:27 -0400

    Refactor majority of code; implement Helpers class

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
+math.randomseed(os.time())
+
 local ScriptName = "MultipleMarkAndRecall"
 local MarksConfig = "MultipleMarkAndRecall_marks"
 
-local MMAR = {}
-
-MMAR.DefaultConfig =
+local Defaults =
 {
   MinStaffRankMark = 1,
   MinStaffRankMarkRm = 1,
@@ -17,16 +17,18 @@ MMAR.DefaultConfig =
   MsgAlertColour = color.Red
 }
 
-MMAR.SortOrder =
+MMAR =
 {
-  "MinStaffRankMark", "MinStaffRankMarkRm", "MinStaffRankRecall", "MinStaffRankList",
-  "SpellCost", "SkillProgressPoints",
-  "MsgPrefixColour", "MsgGeneralColour", "MsgSuccessColour", "MsgAlertColour"
+  Config = Defaults,
+  SortOrder =
+  {
+    "MinStaffRankMark", "MinStaffRankMarkRm", "MinStaffRankRecall", "MinStaffRankList",
+    "SpellCost", "SkillProgressPoints",
+    "MsgPrefixColour", "MsgGeneralColour", "MsgSuccessColour", "MsgAlertColour"
+  }
 }
-
-MMAR.Config = DataManager.loadConfiguration(ScriptName, MMAR.DefaultConfig, MMAR.SortOrder)
-
-MMAR.Marks = DataManager.loadConfiguration(MarksConfig, {})
+MMAR.Config = DataManager.loadConfiguration(ScriptName, Defaults, MMAR.SortOrder)
+MMAR.Marks = DataManager.loadConfiguration(MarksConfig, { })
 
 MMAR.ChatTypes =
 {
@@ -35,9 +37,10 @@ MMAR.ChatTypes =
   ALERT   = MMAR.Config.MsgAlertColour,
 }
 
-math.randomseed(os.time())
+dofile("server/scripts/custom/tes3mp-mmar-shared/helpers.lua")
+local Helpers = MMAR.Helpers
 
-MMAR.RunMarkOrRecall = function(pid, cmd)
+function MMAR.RunMarkOrRecall(pid, cmd)
   local spell = cmd[1]
 
   local cmdRank = 0
@@ -47,34 +50,28 @@ MMAR.RunMarkOrRecall = function(pid, cmd)
     cmdRank = MMAR.Config.MinStaffRankRecall
   end
 
-  if not HasPermission(pid, cmdRank) then
+  if not Helpers:HasPermission(pid, cmdRank) then
     return
   end
 
   local spellUpper = spell:gsub("^%l", string.upper)
-
   local markName = tableHelper.concatenateFromIndex(cmd, 2)
-  local mark = MMAR.Marks[markName]
 
-  if not HasSpell(pid, spell) then
-    ChatMsg(pid, string.format("You do not have the %s spell!", spellUpper), MMAR.ChatTypes.ALERT)
-  elseif markName == "" then
-    ChatMsg(pid, "Please supply a mark name!\nIf you do not know any marks, do \"/ls\"", MMAR.ChatTypes.ALERT)
-  elseif spell == "recall" and not mark then
-    ChatMsg(pid, string.format("Recall failed; mark \"%s\" doesn't exist!", markName), MMAR.ChatTypes.ALERT)
-  elseif SpellSuccess(pid, spellUpper) then
+  if not Helpers:HasSpell(pid, spell) then
+    Helpers:ChatMsg(pid, string.format("You do not have the %s spell!", spellUpper), MMAR.ChatTypes.ALERT)
+  elseif Helpers:CheckMarkExists(pid, spellUpper, markName) and Helpers:SpellSuccess(pid, spellUpper) then
     if spell == "mark" then
-      SetMark(pid, markName)
+      Helpers:SetMark(pid, markName)
+      DataManager.saveConfiguration(MarksConfig, MMAR.Marks)
     elseif spell == "recall" then
-      DoRecall(pid, markName)
+      Helpers:DoRecall(pid, markName)
     end
-
-      DataManager.saveConfiguration(MarksConfig, MMAR.Marks, MMAR.SortOrder)
   end
 end
 
-MMAR.RmMark = function(pid, cmd)
-  if not HasPermission(pid, MMAR.Config.MinStaffRankMarkRm) then
+function MMAR.RmMark(pid, cmd)
+  if not Helpers:HasPermission(pid, MMAR.Config.MinStaffRankMarkRm)
+    or not Helpers:CheckMarkExists(pid, "MarkRM") then
     return
   end
 
@@ -83,18 +80,19 @@ MMAR.RmMark = function(pid, cmd)
   MMAR.Marks[markName] = nil
   tableHelper.cleanNils(MMAR.Marks)
 
-  ChatMsg(pid, string.format("Mark \"%s\" has been deleted by %s!", markName, tes3mp.GetName(pid)), MMAR.ChatTypes.ALERT, true)
+  DataManager.saveConfiguration(MarksConfig, MMAR.Marks)
+  Helpers:ChatMsg(pid, string.format("Mark \"%s\" has been deleted by %s!", markName, tes3mp.GetName(pid)), MMAR.ChatTypes.ALERT, true)
 end
 
-MMAR.ListMarks = function(pid)
-  if not HasPermission(pid, MMAR.Config.MinStaffRankList) then
+function MMAR.ListMarks(pid)
+  if not Helpers:HasPermission(pid, MMAR.Config.MinStaffRankList) then
     return
   end
 
   if tableHelper.isEmpty(MMAR.Marks) then
-    ChatMsg(pid, "There are no marks set.", MMAR.ChatTypes.GENERAL)
+    Helpers:ChatMsg(pid, "There are no marks set.", MMAR.ChatTypes.GENERAL)
   else
-    ChatMsg(pid, "Marks:", MMAR.ChatTypes.GENERAL)
+    Helpers:ChatMsg(pid, "Marks:", MMAR.ChatTypes.GENERAL)
 
     local sortedMarkNames = { }
     local sortedMarkCells = { }
@@ -105,16 +103,16 @@ MMAR.ListMarks = function(pid)
     table.sort(sortedMarkNames)
 
     for _, name in ipairs(sortedMarkNames) do
-      ChatMsg(pid, string.format("%s %s(%s)", name, MMAR.ChatTypes.SUCCESS, sortedMarkCells[name]), MMAR.ChatTypes.GENERAL)
+      Helpers:ChatMsg(pid, string.format("%s %s(%s)", name, MMAR.ChatTypes.SUCCESS, sortedMarkCells[name]), MMAR.ChatTypes.GENERAL)
       -- todo: perhaps allow specifically this message to be customized?
     end
   end
 
-  ChatMsg(pid, string.format("There are currently %d marks.", tableHelper.getCount(MMAR.Marks)), MMAR.ChatTypes.GENERAL)
+  Helpers:ChatMsg(pid, string.format("There are currently %d marks.", tableHelper.getCount(MMAR.Marks)), MMAR.ChatTypes.GENERAL)
 end
 
-MMAR.Back = function(pid)
-  if not HasPermission(pid, MMAR.Config.MinStaffRankRecall) then
+function MMAR.Back(pid)
+  if not Helpers:HasPermission(pid, MMAR.Config.MinStaffRankRecall) then
     return
   end
 
@@ -122,14 +120,14 @@ MMAR.Back = function(pid)
 
   local loc = player.data.customVariables.mmarBack
 
-  if not loc then
-    ChatMsg(pid, "Unable to find previous location in file!", MMAR.ChatTypes.ALERT)
+  if loc then
+    Helpers:ChatMsg(pid, "Unable to find previous location in file!", MMAR.ChatTypes.ALERT)
     return
   end
 
-  if SpellSuccess(pid, "Recall") then
-    ChatMsg(pid, "Returned back to previous location!", MMAR.ChatTypes.SUCCESS)
-    player.data.customVariables.mmarBack = SwapPlayerLocDataWithTable(pid, loc)
+  if Helpers:SpellSuccess(pid, "Recall") then
+    Helpers:ChatMsg(pid, "Returned back to previous location!", MMAR.ChatTypes.SUCCESS)
+    player.data.customVariables.mmarBack = Helpers:SwapPlayerLocDataWithTable(pid, loc)
   end
 end
 
